5ba7063872d681dbf71613e3ea14f855bfa1ff17
QuanGe <zhang_ru_quan@163.com>
2016-05-11 14:11:20 +0800

    摇一摇可以清空画布
    摇一摇可以清空画布

--- src/com/quange/jhds/DrawActivity.java
+++ src/com/quange/jhds/DrawActivity.java
@@ -9,9 +9,15 @@ import com.quange.views.BrushView;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Service;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -22,12 +28,14 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 
-@SuppressLint("NewApi") public class DrawActivity extends Activity implements OnColorChangedListener{
+@SuppressLint("NewApi") public class DrawActivity extends Activity implements OnColorChangedListener, SensorEventListener{
 	private BrushView brushView;
 	private RelativeLayout selectBtn;
 	private GradientDrawable brushColor;
 	private RelativeLayout.LayoutParams brushFrame;
 	private View brushIcon;
+	private SensorManager sensorManager = null;  
+	private Vibrator vibrator = null;  
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
@@ -61,7 +69,8 @@ import android.widget.RelativeLayout;
         brushFrame.addRule(RelativeLayout.CENTER_IN_PARENT); 
         selectBtn.addView(brushIcon, brushFrame);
         
-        
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);  
+        vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);  
     }
 	
 	private void showDeleteDialog() {
@@ -102,4 +111,47 @@ import android.widget.RelativeLayout;
         return super.onKeyDown(keyCode, event);
  
     }
+	 @Override  
+	    protected void onPause()  
+	    {  
+	        super.onPause();  
+	        sensorManager.unregisterListener(this);  
+	    }  
+	  
+	    @Override  
+	    protected void onResume()  
+	    {  
+	        super.onResume();  
+	        sensorManager.registerListener(this,  
+	                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),  
+	                SensorManager.SENSOR_DELAY_NORMAL);  
+	    }  
+	@Override  
+    public void onAccuracyChanged(Sensor sensor, int accuracy)  
+    {  
+        //当传感器精度改变时回调该方法，Do nothing.  
+    }  
+  
+    @Override  
+    public void onSensorChanged(SensorEvent event)  
+    {  
+  
+        int sensorType = event.sensor.getType();  
+        //values[0]:X轴，values[1]：Y轴，values[2]：Z轴  
+        float[] values = event.values;  
+        if (sensorType == Sensor.TYPE_ACCELEROMETER)  
+        {  
+            if ((Math.abs(values[0]) > 17 || Math.abs(values[1]) > 17 || Math  
+                    .abs(values[2]) > 17))  
+            {  
+                Log.d("sensor x ", "============ values[0] = " + values[0]);  
+                Log.d("sensor y ", "============ values[1] = " + values[1]);  
+                Log.d("sensor z ", "============ values[2] = " + values[2]);  
+                brushView.clearAll();
+                //摇动手机后，再伴随震动提示~~  
+                vibrator.vibrate(500);  
+            }  
+  
+        }  
+    }  
 }
