1d49bcd506d777bfa1cb760f49645522fbb8c1df
QuanGe <zhang_ru_quan@163.com>
2016-05-31 20:05:18 +0800

    splash
    splash

---
+++ src/com/quange/jhds/SplashActivity.java
@@ -0,0 +1,181 @@
+package com.quange.jhds;
+
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+import com.quange.views.JHDSGuideView;
+import com.quange.views.JHDSTextGuideView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.LayoutParams;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+
+public class SplashActivity extends Activity implements OnClickListener{
+	 @ViewInject(R.id.guidePages)
+	 private ViewPager guidePages;
+	 @ViewInject(R.id.guidePagesBox)
+	 private RelativeLayout guidePagesBox;
+	// 底部小点的图片
+	 private ImageView[] points;
+	// 记录当前选中位置
+	 private int currentIndex;
+	 private String[] allInfo = {"给你一块画板\n画出你的天空","海量简画教程\n持续不断更新","临摹优秀作品\n提示自己笔格","手机摇一摇\n重新来画","音量键"};
+	 private int iconArray[] = { R.drawable.splash0, R.drawable.splash1,
+				R.drawable.splash2, R.drawable.splash3 };
+	 public class guideAdapter extends PagerAdapter {
+
+			private View mCurrentView;
+		    
+		    @Override
+		    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+		        mCurrentView = (View)object;
+		    }
+		                                             
+		    public View getPrimaryItem() {
+		        return mCurrentView;
+		    }
+			public boolean isViewFromObject(View view, Object o) {
+				return view == o;
+			}
+
+			public int getCount() {
+				return allInfo.length;
+			}
+
+			public void destroyItem(View container, int position, Object object) {
+				((ViewPager) container).removeView((View) object);
+			}
+
+			public Object instantiateItem(View container, int position) {
+			
+				if(position!=4)
+				{
+					JHDSGuideView photoView = new JHDSGuideView(container.getContext());
+					photoView.contentImageView.setImageResource(iconArray[position]);
+					photoView.tv_info.setText(allInfo[position]);
+		            ((ViewPager) container).addView(photoView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		           
+		            return photoView;
+				}
+				else
+				{
+					JHDSTextGuideView photoView = new JHDSTextGuideView(container.getContext());
+					//photoView.tv_info.setText(allInfo[position]);
+		            ((ViewPager) container).addView(photoView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		           
+		            return photoView;
+				}
+			}
+
+
+		}
+	 public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_splash);
+			ViewUtils.inject(this); // 注入view和事件
+			guidePages.setAdapter(new guideAdapter() );
+			initPoint();
+			//guidePagesBox.setVisibility(View.GONE);
+			guidePages.addOnPageChangeListener(new OnPageChangeListener() {
+
+				@Override
+				public void onPageSelected(int position) {
+					// TODO Auto-generated method stub
+					setCurDot(position);
+				}
+
+				@Override
+				public void onPageScrolled(int arg0, float arg1, int arg2) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public void onPageScrollStateChanged(int arg0) {
+					// TODO Auto-generated method stub
+				}
+			});
+			
+		 }
+	 
+	 /**
+	     * 初始化底部小点
+	     */
+	    private void initPoint() {
+	        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ll);
+
+	        points = new ImageView[5];
+
+	        // 循环取得小点图片
+	        for (int i = 0; i < 5; i++) {
+	            // 得到一个LinearLayout下面的每一个子元素
+	            points[i] = (ImageView) linearLayout.getChildAt(i);
+	            // 默认都设为灰色
+	            points[i].setEnabled(true);
+	            // 给每个小点设置监听
+	            points[i].setOnClickListener(this);
+	            // 设置位置tag，方便取出与当前位置对应
+	            points[i].setTag(i);
+	        }
+
+	        // 设置当面默认的位置
+	        currentIndex = 0;
+	        // 设置为白色，即选中状态
+	        points[currentIndex].setEnabled(false);
+	    }
+
+	    
+
+	    @Override
+	    public void onClick(View v) {
+	        int position = (Integer) v.getTag();
+	        setCurView(position);
+	        setCurDot(position);
+	    }
+
+	    /**
+	     * 设置当前页面的位置
+	     */
+	    private void setCurView(int position) {
+	        if (position < 0 || position >= 5) {
+	            return;
+	        }
+	        guidePages.setCurrentItem(position);
+	    }
+
+	    /**
+	     * 设置当前的小点的位置
+	     */
+	    private void setCurDot(int positon) {
+	        if (positon < 0 || positon > 5 - 1 || currentIndex == positon) {
+	            return;
+	        }
+	        points[positon].setEnabled(false);
+	        points[currentIndex].setEnabled(true);
+
+	        currentIndex = positon;
+	    }
+	    
+	    private void startMainActivity() {
+			 final Intent it = new Intent(this, MainActivity.class);
+			 startActivity(it);
+			 
+		 }
+	    
+	    @OnClick(R.id.btn_skip_splashimage)
+	    private void skipToMain(View view)
+	    {
+	    	startMainActivity();
+	    }
+}
---
+++ src/com/quange/views/JHDSTextGuideView.java
@@ -0,0 +1,59 @@
+package com.quange.views;
+
+import com.quange.jhds.AppCommon;
+import com.quange.jhds.MainActivity;
+import com.quange.jhds.R;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+@SuppressLint("NewApi") public class JHDSTextGuideView extends RelativeLayout {
+	public Button tv_info;
+	public JHDSTextGuideView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		LayoutInflater.from(context).inflate(R.layout.view_textguide, this,true);
+		buildView();
+	}
+
+	public JHDSTextGuideView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		LayoutInflater.from(context).inflate(R.layout.view_textguide, this,true);
+		buildView();
+	}
+	
+	public void buildView()
+	{
+		tv_info = (Button)findViewById(R.id.tv_info);
+		
+		GradientDrawable btnColor =  new GradientDrawable();
+		btnColor.setCornerRadius( 5*AppCommon.getInstance().metrics.density);
+		btnColor.setColor(0xffDF0526);
+    	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+    		tv_info.setBackground(btnColor);
+    	else
+    		tv_info.setBackgroundDrawable(btnColor);
+		tv_info.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getContext(), MainActivity.class);
+				
+				getContext().startActivity(intent);
+			}
+		});
+	}
+
+}
