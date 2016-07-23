e1244c46c5b1527ce2ea760f55b8784490704607
QuanGe <zhang_ru_quan@163.com>
2016-07-23 22:54:02 +0800

    表情显示bug

--- src/com/quange/views/EmojiTextView.java
+++ src/com/quange/views/EmojiTextView.java
@@ -1,9 +1,14 @@
 package com.quange.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.text.Html;
 import android.text.Html.ImageGetter;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -24,10 +29,51 @@ public class EmojiTextView extends TextView {
 		super(context, attrs, defStyle);
 		this.context = context;
 	}
+	public class StickerSpan extends ImageSpan {  
+		  
+	    public StickerSpan(Drawable b, int verticalAlignment) {  
+	        super(b, verticalAlignment);  
+	  
+	    }  
+	  
+	    @Override  
+	    public void draw(Canvas canvas, CharSequence text,  
+	                     int start, int end, float x,  
+	                     int top, int y, int bottom, Paint paint) {  
+	        Drawable b = getDrawable();  
+	        canvas.save();  
+	        int transY = bottom - b.getBounds().bottom - 0;  
+	        if (mVerticalAlignment == ALIGN_BASELINE) {  
+	            int textLength = text.length();  
+	            for (int i = 0; i < textLength; i++) {  
+	                if (Character.isLetterOrDigit(text.charAt(i))) {  
+	                    transY -= paint.getFontMetricsInt().descent;  
+	                    break;  
+	                }  
+	            }  
+	        }  
+	        canvas.translate(x, transY);  
+	        b.draw(canvas);  
+	        canvas.restore();  
+	    }  
+	}  
 	
 	public void setEmojiText(String text) {
 		text = EmojiUtils.convertTag(text);
-		CharSequence spanned = Html.fromHtml(text, emojiGetter, null);
+		Spanned spanned = Html.fromHtml(text, emojiGetter, null);
+		
+		if (spanned instanceof SpannableStringBuilder) {  
+	           ImageSpan[] imageSpans = spanned.getSpans(0, spanned.length(), ImageSpan.class);  
+	           for (ImageSpan imageSpan : imageSpans) {  
+	               int start = spanned.getSpanStart(imageSpan);  
+	               int end = spanned.getSpanEnd(imageSpan);  
+	               Drawable d = imageSpan.getDrawable();  
+	               StickerSpan newImageSpan = new StickerSpan(d, ImageSpan.ALIGN_BASELINE);  
+	               ((SpannableStringBuilder) spanned).setSpan(newImageSpan, start, end, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);  
+	               ((SpannableStringBuilder) spanned).removeSpan(imageSpan);  
+	           }  
+	    }  
+		
 		setText(spanned);
 	}
 	
